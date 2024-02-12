5eddb575f472dc9053fc87084dc11aec747cdbab
jperdue21 <john_perdue@brown.edu>
2024-02-12 14:17:14 -0500

    feat: load handler complete, untested

--- src/main/java/edu/brown/cs/student/main/handlers/LoadHandler.java
+++ src/main/java/edu/brown/cs/student/main/handlers/LoadHandler.java
@@ -1,15 +1,20 @@
 package edu.brown.cs.student.main.handlers;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
 import edu.brown.cs.student.main.parser.Parser;
 import edu.brown.cs.student.main.parser.creatorTypes.StringListStrategy;
 import edu.brown.cs.student.main.server.Server;
 import edu.brown.cs.student.main.server.ServerState;
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -54,10 +59,10 @@ public class LoadHandler implements Route {
 
   /**
    * Response object to send given a request
-   * @param success success
+   * @param type success
    * @param message message about outcome
    */
-  public record LoadSuccessResponse(String success, String message){
+  public record LoadSuccessResponse(String type, String message){
 
     /**
      * Successful response data
@@ -67,16 +72,64 @@ public class LoadHandler implements Route {
       this("success",message);
     }
 
+    /**
+     * @return response as a json
+     */
     String serialize(){
-      return null;
+
+      // Instead of taking in map, build map given result and message then serialize
+      Map<String, Object> responseMap = new HashMap<>();
+      responseMap.put("result", this.type);
+      responseMap.put("message", this.message);
+      Type stringObjectMap = Types.newParameterizedType(Map.class, String.class, Object.class);
+
+      try {
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<Map<String, Object>> adapter = moshi.adapter(stringObjectMap);
+        return adapter.toJson(responseMap);
+      } catch (Exception e) {
+        e.printStackTrace();
+        throw e;
+      }
 
     }
+  }
 
+  /**
+   * Response object to send given a request
+   * @param type failure
+   * @param message message about outcome
+   */
+  public record LoadFailureResponse(String type, String message){
 
+    /**
+     * Successful response data
+     * @param message message to send with success
+     */
+    public LoadFailureResponse(String message){
+      this("failure",message);
+    }
 
+    /**
+     * @return response as a json
+     */
+    String serialize(){
 
+      Map<String, Object> responseMap = new HashMap<>();
+      responseMap.put("result", this.type);
+      responseMap.put("message", this.message);
+      Type stringObjectMap = Types.newParameterizedType(Map.class, String.class, Object.class);
 
+      try {
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<Map<String, Object>> adapter = moshi.adapter(stringObjectMap);
+        return adapter.toJson(responseMap);
+      } catch (Exception e) {
+        e.printStackTrace();
+        throw e;
+      }
 
+    }
   }
 
 
