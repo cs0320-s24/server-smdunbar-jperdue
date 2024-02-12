45d10a73bad1febae2ccb5f5159a3fbcc49feb72
jperdue21 <john_perdue@brown.edu>
2024-02-12 10:15:32 -0500

    feat: created serverstate class for handlers to edit

---
+++ src/main/java/edu/brown/cs/student/main/server/ServerState.java
@@ -0,0 +1,56 @@
+package edu.brown.cs.student.main.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerState {
+
+    private ArrayList<List<String>> csvData;
+    private String filepath;
+
+    /**
+     * Constructor for state does nothing. Will set values in their own methods
+     */
+    public ServerState() {}
+
+    /**
+     * Takes an input filepath and sets it to the filepath field of the state
+     * @param filepath String filepath
+     */
+    public void setFilepath(String filepath){
+        this.filepath = filepath;
+    }
+
+    /**
+     * Takes input parsed csvData and assigns it to the state
+     * @param csvData list<list<string>> parsed data
+     */
+    public void setCsvData(ArrayList<List<String>> csvData){
+        this.csvData = csvData;
+    }
+
+    /**
+     * Getter for filepath string
+     * @return string filepath
+     */
+    public String getFilepath(){
+        return this.filepath;
+    }
+
+    /**
+     * Returns csv data
+     * @return csv data field
+     */
+    public ArrayList<List<String>> getCsvData(){
+        return this.csvData;
+    }
+
+    /**
+     * Clears state data and sets both values to null.
+     */
+    public void clearData(){
+        this.filepath = null;
+        this.csvData = null;
+    }
+
+}
